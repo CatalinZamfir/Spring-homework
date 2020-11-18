4d710ff956f9a21b79abfd8cb3a15f1e33181048
Zamfir Catalin <catalin_provideo@yahoo.com>
2020-11-18 14:39:30 +0200

    homework done

---
+++ src/main/java/org/example/config/InvoiceService.java
@@ -0,0 +1,44 @@
+package org.example.config;
+
+import org.example.entity.Invoices;
+import org.example.repository.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class InvoiceService {
+        @Autowired
+        public Repository repository;
+
+        public Invoices saveInvoice (Invoices invoice){
+            return repository.save(invoice);
+        }
+
+        public List<Invoices> getInvoices(){
+            return repository.findAll();
+        }
+
+        public Optional<Invoices> findById (int id){
+        return repository.findById(id);
+        }
+
+        public String deleteById (int id){
+        repository.deleteById(id);
+        return "invoice removed " + id;
+    }
+
+    public Invoices updateInvoice (Invoices invoice){
+        Invoices existingInvoice = repository.findById(invoice.getId()).orElse(null);
+                existingInvoice.setCompany(invoice.getCompany());
+                existingInvoice.setDescription(invoice.getDescription());
+                existingInvoice.setPrice(invoice.getPrice());
+                return repository.save(existingInvoice);
+    }
+}
---
+++ src/main/java/org/example/controller/RestControllers.java
@@ -0,0 +1,45 @@
+package org.example.controller;
+
+import org.example.config.InvoiceService;
+import org.example.entity.Invoices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class RestControllers {
+
+    @Autowired
+    public InvoiceService invoiceService;
+
+
+    @PostMapping("/addInvoice")
+    public Invoices addInvoice (@RequestBody Invoices invoice){
+        return invoiceService.saveInvoice(invoice);
+    }
+    
+    @GetMapping("/getInvoiceById/{id}")
+    public Optional<Invoices> getInvoiceById (@PathVariable int id){
+        return invoiceService.findById(id);
+    }
+    @GetMapping ("/getInvoices")
+    public List<Invoices> getInvoices(){
+        return invoiceService.getInvoices();
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public String deleteById (@PathVariable int id){
+        return invoiceService.deleteById(id);
+    }
+
+    @PutMapping ("/update")
+    public Invoices updateInvoice (@RequestBody Invoices invoice){
+        return invoiceService.saveInvoice(invoice);
+    }
+
+
+}
